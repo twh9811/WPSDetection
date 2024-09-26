3d2b91094a3f5ee04ca51e41959dc69d4f5db0d9
Travis Hill <twh9811@rit.edu>
2024-09-26 17:37:47 -0400

    more testing, no worky, out of wigle queries

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import geopy.distance
 
 
 API_KEY = os.getenv("API_KEY")
@@ -22,7 +23,7 @@ def google_wps_triangulation(bssids, desired_mac):
     data["wifiAccessPoints"].append(prioritized_access_point)
     
     for bssid in bssids:
-        access_point = {"macAddress":bssid, "signalStrength":-75, "signalToNoiseRatio":20}
+        access_point = {"macAddress":bssid, "signalStrength":-75, "signalToNoiseRatio":15}
 
         data["wifiAccessPoints"].append(access_point)
     
@@ -45,9 +46,9 @@ def bssid_collection_via_wigle():
     # North Greece 
     max_lat = 43.254727440238206
     max_long = -77.73275248513913
-    # Pittsford
-    min_lat = 43.09236979150439
-    min_long = -77.51533066218884
+    # West Irondequoit
+    min_lat = 43.22439607883378
+    min_long = -77.59918017155313
     
     # Establish query parameters
     params = {
@@ -55,29 +56,29 @@ def bssid_collection_via_wigle():
         'latrange2': max_lat,
         'longrange1': min_long,
         'longrange2': max_long,
-        'resultsPerPage': 50
+        'lastupdtstart': '2024-01-01',
+        'lastupdtend': '2024-12-31',
+        'resultsPerPage': 100
     }
 
     # Make the request
     response = requests.get(WIGLE_URL, headers=headers, params=params)
     
     if response.status_code == 200:
-        bssids = {}
+        bssids = []
         response = response.json()
         # Parses json to check query success flag
         if response.get("success"):
             results = response.get("results")
             for network in results:
                 bssid = network.get("netid")
+                coordinates = (network.get("trilat"), network.get("trilong"))
                 bssids.append(bssid)
-                # ssid = network.get("ssid")
-                # coordinates = [network.get("trilat"), network.get("trilong")]
-                #print("BSSID: ", bssid, " SSID: ", ssid, " Coordinates: ", coordinates)
             return bssids
         else:
             print("No Networks Found")
     else:
-        print("Query Error:", response.status_code, response.reason, response.text.)
+        print("Query Error:", response.status_code, response.reason, response.text)
 
 def main():
     test_mac = "E8-65-38-02-4A-6F"
