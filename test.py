fb76786473d2c1b1e40169c151ef63f7fd59785f
Travis Hill <twh9811@rit.edu>
2024-10-22 16:14:47 -0400

    Out of queries, updated to try and triangulate based on close coords

--- test.py
+++ test.py
@@ -9,15 +9,10 @@ WIGLE_API_KEY = os.getenv("WIGLE_API_KEY")
 MAPS_URL = "https://www.googleapis.com/geolocation/v1/geolocate?key=" + API_KEY
 WIGLE_URL = "https://api.wigle.net/api/v2/network/search"
 
-def google_wps_triangulation(desired_mac, close_bssids=None):
-    """
-    Provides Google Maps WPS a list of BSSIDs in a specific r egion and attempts to locate a priority BSSID in that region.
-    Loops until accuracy is improved to threshold.
-    """
-    attempt = 0
+def google_wps_triangulation(desired_mac, desired_location=None):
     accuracy_threshold = 120
-    accurate_bssids =  set()
-    while attempt < 10:
+    close_locations = []
+    while len(close_locations) < 3:
 
         data = {
             "wifiAccessPoints": []
@@ -31,54 +26,40 @@ def google_wps_triangulation(desired_mac, close_bssids=None):
         # data["wifiAccessPoints"].append({"macAddress":"f8-5b-3b-e0-05-fa", "signalStrength":-57, "signalToNoiseRatio":40})
         # data["wifiAccessPoints"].append({"macAddress":"08-36-c9-95-ee-71", "signalStrength":-57, "signalToNoiseRatio":40})
         # data["wifiAccessPoints"].append({"macAddress":"a0-55-1f-68-1d-39", "signalStrength":-57, "signalToNoiseRatio":40})
-        if(close_bssids is not None):
-            for bssid in close_bssids:
-                access_point = {"macAddress":bssid, "signalStrength":-75, "signalToNoiseRatio":25}
-                data["wifiAccessPoints"].append(access_point)
-        else:
+        if(desired_location is None):
             bssids = bssid_collection_via_wigle()
-            for bssid in bssids:
-                access_point = {"macAddress":bssid, "signalStrength":-90, "signalToNoiseRatio":15}
-                data["wifiAccessPoints"].append(access_point)
+        else:
+            bssids = bssid_collection_via_wigle(triangulated_location[0], triangulated_location[1])
+            
+        for bssid in bssids:
+            access_point = {"macAddress":bssid, "signalStrength":-90, "signalToNoiseRatio":15}
+            data["wifiAccessPoints"].append(access_point)
             
         response = requests.post(MAPS_URL, json=data)
 
         received_location = response.json()
-        
-        # Check accuracy. If its good, add these to a list with a better signal strength to signify these should be closer to the desired location.
+
         if received_location['accuracy'] < accuracy_threshold:
             print("Good Accuracy", received_location)
-            for bssid in bssids:
-                accurate_bssids.add(bssid)
+            coordinates = received_location['location']
+            close_locations.append([coordinates['lat'],coordinates['long']])
         else:
-            # Otherwise ignore bssids, try again.
             print("Too High", received_location)
-        attempt += 1
-        
-    data = {
-        "wifiAccessPoints": []
-    }
-        
-    prioritized_access_point = {"macAddress":desired_mac, "signalStrength":-57, "signalToNoiseRatio":40}
-    data["wifiAccessPoints"].append(prioritized_access_point)
-    for bssid in accurate_bssids:
-        access_point = {"macAddress":bssid, "signalStrength":-85, "signalToNoiseRatio":20}
-        data["wifiAccessPoints"].append(access_point)
-    response = requests.post(MAPS_URL, json=data)
 
-    received_location = response.json()
-    print(received_location)
-    return accurate_bssids
+    central_lat = sum(location[0] for location in close_locations)
+    central_long = sum(location[1] for location in close_locations)
+    triangulated_location = [central_lat, central_long]
+    return triangulated_location
+    
+# Irondequoit (43.21313319463426, -77.5818301738431)
+def bssid_collection_via_wigle(lat=43.21, long=-77.58):
 
-def bssid_collection_via_wigle():
-    """
-    Creates a box outlining a specific geographic region, returns bssids in that location.
-    """
     mod_range = [-.1, .1]
     lat_min_modifier = random.uniform(mod_range[0], mod_range[1])
     long_min_modifier = random.uniform(mod_range[0], mod_range[1])
-    lat_max_modifier = random.uniform(mod_range[0], mod_range[1])
-    long_max_modifier = random.uniform(mod_range[0], mod_range[1])
+    # Use min modifier as the range (therefore cant go lower than this)
+    lat_max_modifier = random.uniform(lat_min_modifier, mod_range[1])
+    long_max_modifier = random.uniform(long_min_modifier, mod_range[1])
     
     print(lat_min_modifier, long_min_modifier, lat_max_modifier, long_max_modifier)
     # Set headers with your API key
@@ -86,11 +67,10 @@ def bssid_collection_via_wigle():
         "Authorization": "Basic " + WIGLE_API_KEY
     }
     
-    # Irondequoit (43.21313319463426, -77.5818301738431)
-    min_lat = 43.21 + lat_min_modifier
-    min_long = -77.58 + long_min_modifier
-    max_lat = 43.21 + lat_max_modifier
-    max_long = -77.58 + long_max_modifier
+    min_lat = lat + lat_min_modifier
+    min_long = long + long_min_modifier
+    max_lat = lat + lat_max_modifier
+    max_long = long + long_max_modifier
     
     # Establish query parameters
     params = {
@@ -102,9 +82,11 @@ def bssid_collection_via_wigle():
         'lastupdtend': '2024-12-31',
         'resultsPerPage': 100
     }
-
+    
     # Make the request
+    print("Query Request Sent")
     response = requests.get(WIGLE_URL, headers=headers, params=params)
+    print("Answer Received")
     
     if response.status_code == 200:
         bssids = []
@@ -124,13 +106,12 @@ def bssid_collection_via_wigle():
 def main():
     max_tries = 3
     test_mac = "80-78-71-c7-f4-96"
-    close_bssids = set()
+    triangulated_location = None
     for i in range(max_tries):
-        print("Try 1")
-        if(i == 0):
-            curr_set = google_wps_triangulation(test_mac)
+        print("Try", i+1)
+        if(triangulated_location is None):
+            triangulated_location = google_wps_triangulation(test_mac)
         else:
-            curr_set = google_wps_triangulation(test_mac, close_bssids)
-        close_bssids.update(curr_set)
+            triangulated_location = google_wps_triangulation(test_mac, triangulated_location)
 if __name__ == "__main__":
     main()
